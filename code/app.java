a00d8b5dadde5cbf1ab63d31182a3de43d677b62
Leyson David Asprilla Mosquera <145991327+Leyson-Gith@users.noreply.github.com>
2024-05-22 17:03:21 -0500

    Actualizacion 22/05/2024 4:56 pm
    1.Crear la clase app
    2.Creando el main
    3. creando el privado del formato de fnt_menu con codigo while
    3. creando el formato  privado del paciente fnt_paciente con los pronombres
    4. Creando el linkedLinst conectando con el cls_pacientes llamado paciente

--- code/app.java
+++ code/app.java
@@ -1,5 +1,31 @@
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
 public class app {
-    
-    private String sol;
-    
+    static LinkedList<cls_pasientes> paciente = new LinkedList<cls_pasientes>();
+
+  public static void main(String[] args) {
+  fnt_menuPrincipal(true);
+  }
+
+
+  private static void fnt_pacientes () {
+    String nombre= JOptionPane.showInputDialog(null, "NOMBRE:");
+    String id= JOptionPane.showInputDialog(null, "ID:");
+    int edad= Integer.parseInt(JOptionPane.showInputDialog(null, "EDAD:"));
+    String contacto = JOptionPane.showInputDialog(null, "CONTACTO:");
+    String historia= JOptionPane.showInputDialog(null, "HISTORIA:");
+    paciente.add(new cls_pasientes(nombre, id, edad, contacto, historia));
+  }
+  private static void fnt_menuPrincipal (boolean m){
+    while (m== true) {
+        String opcionStr = JOptionPane.showInputDialog(null,"MENUPRINCIPAL \n \n 1.NUEVO PACIENTE \n 2.NUEVA CITA \n 3.REPORTE \n 4. Salir");
+        if(opcionStr.equals("1")) {
+              fnt_pacientes();
+        }
+    }
+
+  }
+
 }
